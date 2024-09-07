e9bcd0d65a2e695768c0c94bef8dad7ccf5059f1
fiona <fiona.mburu@student.moringaschool.com>
2024-09-08 02:21:32 +0300

    pracitice sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(x):
+    if x <= 0:
+        print([])
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < x:
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+        print(fibonacci_sequence[:x])
